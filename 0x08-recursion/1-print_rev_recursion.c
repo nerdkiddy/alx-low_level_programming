2a2d0721fdfcad92422eea8ede8457f06b8caaf5
Jordan Amos <amosjordan7@gmail.com>
2022-09-28 15:02:23 -0700

    recursions 1 commit

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,28 +1,24 @@
-include "main.h"
+#include "main.h"
 
 /**
- * _print_rev_recursion - prints a string in reverse
- *
- * @s: pointer block of memory to fill
- *
- * Return: void
- *
- **/
-
+ *_print_rev_recursion - prints a string in reverse
+ *@s: pointer block of memory to fill
+ *Return: void
+ */
 
 
 void _print_rev_recursion(char *s)
 
 {
 
-	if (*s) /*If statement*/
+if (*s) /*If statement*/
 
-	{
+{
 
-		_print_rev_recursion(s + 1); /*add s*/
+_print_rev_recursion(s + 1); /*add s*/
 
-		_putchar(*s); /*print s*/
+_putchar(*s); /*print s*/
 
-	}
+}
 
 }
