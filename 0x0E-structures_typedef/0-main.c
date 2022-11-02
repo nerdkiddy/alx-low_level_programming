30d212913372cde850c6c9dfbdd1e44fdb94e2ed
Jordan Amos <amosjordan7@gmail.com>
2022-11-02 15:19:09 -0700

    struct 0 commit

--- 0x0E-structures_typedef/0-main.c
+++ 0x0E-structures_typedef/0-main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-#include "dog.h"
 
+#include "dog.h"
 
 /**
  * main - check the code
+ *
  * Return: Always 0.
  */
 
@@ -11,18 +12,16 @@ int main(void)
 
 {
 
-    struct dog my_dog;
+struct dog my_dog;
 
 
+my_dog.name = "Poppy";
 
-    my_dog.name = "Poppy";
+my_dog.age = 3.5;
 
-    my_dog.age = 3.5;
+my_dog.owner = "Bob";
 
-    my_dog.owner = "Bob";
+printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
 
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-
-    return (0);
-
+	return (0);
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,6 @@
 #ifndef FILE_DOG
-#define FILE_DOG
 
+#define FILE_DOG
 
 /**
  * struct dog - structure dog
@@ -10,34 +10,26 @@
  */
 
 struct dog
+
 {
+
 	char *name;
 
 	float age;
 
 	char *owner;
+
 };
-
-
 
 typedef struct dog dog_t;
 
-
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
-
 
 void print_dog(struct dog *d);
 
-
-
 dog_t *new_dog(char *name, float age, char *owner);
-
-
 
 void free_dog(dog_t *d);
 
-
-
 #endif
+
